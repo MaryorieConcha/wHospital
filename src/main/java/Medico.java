70ef3e8c9849d281629d7c732164ca75385a6525
MaryorieConcha <m.concha06@ufroamail.cl>
2022-10-17 16:25:09 -0300

    Clases Paciente y Medico

---
+++ src/main/java/Medico.java
@@ -0,0 +1,28 @@
+public class Medico {
+	private String nombre;
+	private int edad;
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Medico{" +
+				"nombre='" + nombre + '\'' +
+				", edad=" + edad +
+				'}';
+	}
+}
